a4579b9739ec4e3c993594fc5996707c80b09c3a
Gavin <gavingray1729@gmail.com>
2015-10-06 12:41:30 +0100

    mostly done

--- varout/experiments.py
+++ varout/experiments.py
@@ -11,12 +11,16 @@
 # to be.
 
 import layers
+import holonets
 import lasagne.layers
 import lasagne.nonlinearities
+import lasagne.updates
 import urllib2
 import imp
+import itertools
+from collections import OrderedDict
 
-def wangDropoutArchitecture(batch_size=128, input_dim=784, output_dim=10,
+def wangDropoutArchitecture(batch_size=1000, input_dim=784, output_dim=10,
                             DropoutLayer=layers.WangGaussianDropout,
                             n_hidden=100):
     l_in = lasagne.layers.InputLayer((batch_size, input_dim))
@@ -33,7 +37,7 @@ def wangDropoutArchitecture(batch_size=128, input_dim=784, output_dim=10,
             nonlinearity=lasagne.nonlinearities.softmax)
     return l_out
 
-def srivastavaDropoutArchitecture(batch_size=128, input_dim=784, output_dim=10,
+def srivastavaDropoutArchitecture(batch_size=1000, input_dim=784, output_dim=10,
                             DropoutLayer=layers.WangGaussianDropout,
                             n_hidden=100):
     l_in = lasagne.layers.InputLayer((batch_size, input_dim))
@@ -47,6 +51,40 @@ def srivastavaDropoutArchitecture(batch_size=128, input_dim=784, output_dim=10,
     l_out = lasagne.layers.DenseLayer(l_drop_2, num_units=output_dim,
             nonlinearity=lasagne.nonlinearities.softmax)
     return l_out
+
+def make_experiment(l_out, dataset, 
+        loss_function=lasagne.objectives.categorical_crossentropy,
+        extra_loss=0.0, limit_alpha=False):
+    """
+    Build a loop for training a model, evaluating loss on training, validation 
+    and test.
+    """
+    expressions = holonets.monitor.Expressions(l_out, dataset, batch_size, 
+            N_train=50000, N_valid=10000, N_test=10000,
+            update_rule=lasagne.updates.adam, loss_function=loss_function, 
+            loss_aggregate=T.mean, extra_loss=extra_loss, learning_rate=0.001)
+    # only add channels for loss and accuracy
+    for deterministic,dataset in itertools.product([True, False, False],
+                                                   ["train", "valid", False]):
+        expressions.add_channel(**expressions.loss(dataset, deterministic))
+        expressions.add_channel(**expressions.accuracy(dataset, deterministic))
+    channels = expressions.build_channels()
+    if limit_alpha:
+        # then add channel to reset all alphas at 1.0
+        alphas = [p for p in lasagne.layers.get_all_params(l_out) 
+                if p.name == "alpha"]
+        alpha_ceiling = theano.function([], alphas, 
+                updates=OrderedDict([(a, T.min([a, 1.0])) for a in alphas]))
+        channels.append({'dataset': 'train',
+                         'eval': lambda x: alpha_ceiling(),
+                         'dimensions': ['Alpha']*len(alphas),
+                         'names': ['alpha {0}'.format(i) for i in range(len(alphas))]})
+    train = holonets.train.Train(channels, 
+            n_batches={'train': N_train//batch_size, 
+                       'valid':N_valid//batch_size, 
+                       'test':N_test//batch_size})
+    loop = holonets.run.EpochLoop(train, dimensions=train.dimensions)
+    return loop
 
 def earlystopping(loop, delta=0.01, max_N=1000, verbose=False):
     """
@@ -74,3 +112,5 @@ def load_data():
                 y_valid=dataset[3],
                 X_test=dataset[4].reshape(-1, 784),
                 y_test=dataset[5])
+
+
